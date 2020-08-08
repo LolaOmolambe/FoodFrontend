2654cf614bfb4365a578cf745aa47c3df00d713d
Omolola <lola.omolambe@gmail.com>
2020-08-08 11:51:49 +0100

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import * as AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'foodclient';
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    AOS.init({
+      duration: 800,
+      easing: 'slide'
+    });
+  }
+
+
+
 }
+
