5e75afc25a77d19444e2d6afe68bc2f6cf7ad83c
Omolola <lola.omolambe@gmail.com>
2020-08-17 13:10:16 +0100

    Pagination

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
 
   isLoading = false;
   totalProducts = 0;
-  postsPerPage = 6;
+  postsPerPage = 8;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
   userIsAuthenticated = false;
